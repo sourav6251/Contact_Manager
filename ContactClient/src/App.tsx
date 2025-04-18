1170c11931967e25becda9c7de975e6ef3ead255
sourav6251 <dassourav3738+github@gmail.com>
2025-04-18 17:22:33 +0530

    New version

---
+++ ContactClient/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-800 h-6 w-10'></div>
+    </>
+  )
+}
+
+export default App
--- src/main/java/com/contact/service/MailService.java
+++
@@ -1,7 +0,0 @@
-package com.contact.service;
-
-public interface MailService{
-
-    
-
-}
